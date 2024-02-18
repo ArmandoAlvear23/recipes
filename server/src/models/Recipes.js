f2bad5317fea9b8dc52afb2897ee9b8c76a53dc3
Armando Alvear <armando.alvear01@outlook.com>
2024-02-17 23:58:36 -0600

    add recipes model

---
+++ server/src/models/Recipes.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+// Create schema for recipe
+const RecipeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  ingredients: [{ type: String, required: true }],
+  instructions: { type: String, required: true },
+  imgURL: { type: String, required: true },
+  cookingTime: { type: Number, required: true },
+  userOwner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+});
+
+// Create model for recipe
+export const RecipeModel = mongoose.model("recipes", RecipeSchema);
