2527edcb8193cbc505b93e670018836163b60a07
Armando Alvear <armando.alvear01@outlook.com>
2024-02-20 23:37:40 -0600

    remove response and add comments

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { useGetUserID } from "../hooks/useGetUserID.js";
 
 export const CreateRecipe = () => {
+  // Get user ID
   const userID = useGetUserID();
 
+  // Navigate hook
+  const navigate = useNavigate();
+
+  // Recipe object
   const [recipe, setRecipe] = useState({
     name: "",
     ingredients: [],
@@ -22,32 +28,37 @@ export const CreateRecipe = () => {
     setRecipe({ ...recipe, [name]: value });
   };
 
+  // Handle specific ingredient in array change
   const handleIngredientChange = (event, idx) => {
+    // Get value inside text input field
     const { value } = event.target;
+    // Get a copy of current ingredients array
     const ingredients = recipe.ingredients;
+    // Change the value of the ingredient at index
     ingredients[idx] = value;
+    // Replace the ingredients array inside recipe object with new array
     setRecipe({ ...recipe, ingredients });
   };
 
+  // Add blank in ingredient array inside of recipe object
   const addIngredient = () => {
     setRecipe({ ...recipe, ingredients: [...recipe.ingredients, ""] });
   };
 
-  const onSubmit = async (event, req, res) => {
+  // On create recipe form submit
+  const onSubmit = async (event) => {
+    // Prevent post refresh
     event.preventDefault();
     try {
-      // Send post request and get response
-      const response = await axios.post(
-        "http://localhost:3001/recipes",
-        recipe
-      );
-      console.log(recipe);
-      res.json(response);
-      console.log(response);
+      // Send post request to add recipe
+      await axios.post("http://localhost:3001/recipes", recipe);
+      // Navigate to home page
+      navigate("/");
     } catch (err) {
-      res.json(err);
+      console.error(err);
     }
   };
+
   return (
     <div className="create-recipe">
       <h2>Create Recipe</h2>
